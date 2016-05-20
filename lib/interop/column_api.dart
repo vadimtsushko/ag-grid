542be0d0105800726b979d57ab4fa2e2e0ec0fb8
Vadim Tsushko <vadimtsushko@gmail.com>
2016-05-20 16:16:08 +0500

    Some functions added

--- lib/interop/column_api.dart
+++ lib/interop/column_api.dart
@@ -7,6 +7,8 @@ class ColumnApi {
   external List<ColumnDef> getColumnState();
   external void resetColumnState();
   external setColumnWidth(key, int newWidth, [bool finished]);
+  external String getDisplayNameForCol(column);
+  external Column getColumn(key);
 }
 /*
 export declare class ColumnApi {
